e7c60082438e0e96ae3bfe43691cc22ed8e337a2
Manoj S Raju <104692407+Manojsraju@users.noreply.github.com>
2026-03-11 23:24:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,41 +1,36 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "id": "8e688842-5c52-4a87-a0f1-427f90d707c9",
-   "metadata": {},
-   "outputs": [],
-   "source": []
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "id": "4d277851-1c1e-4f0c-b104-57c4f4382a83",
-   "metadata": {},
-   "outputs": [],
-   "source": []
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3 (ipykernel)",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.12.7"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 5
-}
+import streamlit as st
+import pandas as pd
+import joblib
+
+model = joblib.load("credit_risk_model.pkl")
+
+st.title("Credit Risk Prediction")
+
+age = st.number_input("Age")
+sex = st.selectbox("Sex", ["male","female"])
+job = st.selectbox("Job",[0,1,2,3])
+housing = st.selectbox("Housing",["own","rent","free"])
+saving_accounts = st.selectbox("Saving Accounts",["little","moderate","rich"])
+checking_account = st.selectbox("Checking Account",["little","moderate","rich"])
+credit_amount = st.number_input("Credit Amount")
+duration = st.number_input("Duration")
+
+if st.button("Predict"):
+    
+    data = pd.DataFrame({
+        "age":[age],
+        "sex":[sex],
+        "job":[job],
+        "housing":[housing],
+        "saving_accounts":[saving_accounts],
+        "checking_account":[checking_account],
+        "credit_amount":[credit_amount],
+        "duration":[duration]
+    })
+
+    prediction = model.predict(data)
+
+    if prediction[0] == 1:
+        st.success("Good Credit Risk")
+    else:
+        st.error("Bad Credit Risk")
